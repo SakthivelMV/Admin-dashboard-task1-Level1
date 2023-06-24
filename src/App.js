1b7824ef3a042d86cdd1e0945c7e79e66499c015
sakthivelm554@gmail.com <sakthivelm554@gmail.com>
2023-06-24 20:06:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { ColorModeContext, useMode } from "./Theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import { Routes, Route } from "react-router-dom";
+import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
+import FAQ from "./scenes/faq";
+import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import GeographyChart from "./components/GeographyChart";
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+<div className="app">
+  <Sidebar/>
+<main className="content">
+  <Topbar/>
+  <Routes>
+    <Route path="/" element={<Dashboard/>}/>
+    <Route path="/team" element={<Team />} />
+    <Route path="/contacts" element={<Contacts/>}/>
+    <Route path="/invoices" element={<Invoices/>}/>
+    <Route path="/form" element={<Form/>}/>
+    {/* <Route path="/calendar" element={<Calendar/>}/> */}
+    <Route path="/Faq" element={<FAQ/>}/>
+    <Route path="/bar" element={<Bar/>}/>
+    <Route path="/pie" element={<Pie/>}/>
+    <Route path="/line" element={<Line/>}/>
+    <Route path="/geography" element={<GeographyChart/>}/>
+
+
+
+
+
+
+
+
+  </Routes>
+  </main>
+</div>
+</ThemeProvider>
+</ColorModeContext.Provider>
+
   );
 }
 
